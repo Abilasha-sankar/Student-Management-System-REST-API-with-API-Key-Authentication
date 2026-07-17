d9ef221fcb0046a73330a876b5a21936f3757b21
Abilasha-sankar <srenuga167@gmail.com>
2026-07-17 21:07:27 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# Telegram Configuration
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
-
-# Download Folder
-DOWNLOAD_FOLDER = r"YOUR_DOWNLOAD_FOLDER"
-
-# NSE Website
-NSE_URL = "https://www.nseindia.com/market-data/pre-open-market-fno"
